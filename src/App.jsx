175b5aade98ea9445520bf8cf320088c45e646ff
Ankit Jha <aajjha9@gmail.com>
2026-01-08 17:59:27 +0530

    import fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Cta from "./components/Cta";
-import Element from "./components/Element";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Programs from "./components/Programs";
-import Video from "./components/Video";
+import {
+  Cta,
+  Element,
+  Features,
+  Footer,
+  Header,
+  Navbar,
+  Programs,
+  Video,
+} from "./components";
 
 function App() {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Cta } from "./Cta";
+export { default as Element } from "./Element";
+export { default as Features } from "./Features";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as Navbar } from "./Navbar";
+export { default as Programs } from "./Programs";
+export { default as Video } from "./Video";
